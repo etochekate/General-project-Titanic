86a7b9ef21cf32946628d230c99871325f39e042
etochekate <etochekate@gmail.com>
2023-01-04 19:47:10 +0600

    ещё несколько строк предобработки

--- main.py
+++ main.py
@@ -39,3 +39,27 @@ df.nunique()
 
 #заменим все отсутствующие NaN на интерполированные значения столбца Age
 df['Age'] = df['Age'].interpolate()
+
+
+#инфо по датасету
+df.info()
+
+#мужчины-женщины на борту
+df.groupby('Sex')['PassengerId'].count()
+
+#классы билетов
+df.groupby('Pclass')['PassengerId'].count()
+
+#описательная статистика возраста пассажиров
+df.Age.describe()
+
+#описательная статистика тарифов билетов
+df.Fare.describe()
+
+#братьев и сестер на борту
+df.SibSp.unique()
+df.SibSp.describe()
+
+#родителей- детей на борту
+df.Parch.unique()
+df.Parch.describe()
