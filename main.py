43d35c39b522ff843da959e6dbfd0d71ef738912
etochekate <etochekate@gmail.com>
2023-01-04 19:44:54 +0600

    несколько строк предобработки

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ df = pd.read_csv('./test.csv')
 #print(df.head(10))
 
 #сумма нулевых значений
-print(df.isnull().sum())
+#print(df.isnull().sum())
+
+#первые 10 строк
+df.head(10)
+
+#сумма нулевых значений
+df.isnull().sum()
+
+#описание числовых колонок
+df.describe()
+
+#размер датасета
+df.shape
+
+#количество уникальных значений в каждой колонке
+df.nunique()
